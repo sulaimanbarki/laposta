93384b095f8006898368869b8300f1ac6c9e5efd
Sulaiman Barki <99899527+sulaimanbarki@users.noreply.github.com>
2023-05-31 13:02:52 +0500

    laravel scheduling and sharing post on social platform

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Schema::defaultStringLength(191);
+        View::composer('main.*', function ($view) {
+            $notifications = cache()->rememberForever('notifications.' . auth()->user()->id, function () {
+                return auth()
+                    ->user()
+                    ->notifications()
+                    ->orderBy('created_at', 'desc')
+                    ->limit(10)
+                    ->get();
+            });
+
+            $view->with('notifications', $notifications);
+        });
+    }
+}
